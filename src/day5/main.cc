af403a91704f701637f21319d0e550fb5a889976
DGaron <dgaron@colostate.edu>
2022-12-05 13:31:34 -0700

    They stocks not crates yaheard

--- src/day5/main.cc
+++ src/day5/main.cc
@@ -15,17 +15,17 @@ size_t whichStack(const size_t &found) {
 
 vector<stack<char> > dealWithThisNonsense(const vector<string> &data) {
     // 0 is just gonna be empty. I am not dealing with -1 everywhere.
-    vector<stack<char> > crates(10);
+    vector<stack<char> > stacks(10);
     for (int i = 7; i >= 0; --i) {
         const string &s = data[i];
         auto found = s.find('[');
         while (found != string::npos) {
             auto stack = whichStack(found);
-            crates[stack].push(s[found+1]);
+            stacks[stack].push(s[found+1]);
             found = s.find('[', found+1);
         }
     }
-    return crates;
+    return stacks;
 }
 
 vector<int> parseMove(const string &s) {
@@ -43,20 +43,20 @@ vector<int> parseMove(const string &s) {
     return coords;
 }
 
-void moveCrates(const string &s, vector<stack<char> > &crates) {
+void moveCrates(const string &s, vector<stack<char> > &stacks) {
     vector<int> coords = parseMove(s);
     // Assignments so I remember
     int numCrates = coords[0];
     int source = coords[1];
     int target = coords[2];
     for (int i = 0; i < numCrates; ++i) {
-        const char crate = crates[source].top();
-        crates[source].pop();
-        crates[target].push(crate);
+        const char crate = stacks[source].top();
+        stacks[source].pop();
+        stacks[target].push(crate);
     }
 }
 
-void moveStacks(const string &s, vector<stack<char> > &crates) {
+void moveStacks(const string &s, vector<stack<char> > &stacks) {
     vector<int> coords = parseMove(s);                                                              
     // Assignments so I remember                                                                    
     int numCrates = coords[0];                                                                      
@@ -64,12 +64,12 @@ void moveStacks(const string &s, vector<stack<char> > &crates) {
     int target = coords[2];
     stack<char> movingCrates;  
     for (int i = 0; i < numCrates; ++i) { 
-        const char crate = crates[source].top();                                                   
-        crates[source].pop();
+        const char crate = stacks[source].top();                                                   
+        stacks[source].pop();
         movingCrates.push(crate);                                                            
     } 
     for (int i = 0; i < numCrates; ++i) {
-        crates[target].push(movingCrates.top());
+        stacks[target].push(movingCrates.top());
         movingCrates.pop();
     }
 }
@@ -82,11 +82,11 @@ int main(int argc, char *argv[]) {
 
     vector<string> data = readFile(argv[1]);
 
-    // Strip off the crates
-    vector<stack<char> > crates = dealWithThisNonsense(data);
+    // Strip off the stacks
+    vector<stack<char> > stacks = dealWithThisNonsense(data);
 
     /* SEE TO BELIEVE
-    for (auto v : crates) {
+    for (auto v : stacks) {
         while(!v.empty()) {
             char c = v.top();
             v.pop();
@@ -100,25 +100,25 @@ int main(int argc, char *argv[]) {
     vector<string> moves(data.begin()+10, data.end());
 
     for (const string &s : moves) {
-        moveCrates(s, crates);
+        moveCrates(s, stacks);
     }
 
     cout << "PART 1 - Top Row: ";
     for (int i = 1; i < 10; ++i) {
-        cout << crates[i].top();
+        cout << stacks[i].top();
     }
     cout << '\n';
 
-    // RESET THE CRATES DUHHHHH
-    crates = dealWithThisNonsense(data);
+    // RESET THEstacks DUHHHHH
+    stacks = dealWithThisNonsense(data);
 
     for (const string &s : moves) {
-        moveStacks(s, crates);
+        moveStacks(s, stacks);
     }    
 
     cout << "PART 2 - Top Row: ";
     for (int i = 1; i < 10; ++i) {                                                                  
-        cout << crates[i].top();                                                                   
+        cout << stacks[i].top();                                                                   
     }                                                                                               
     cout << '\n';
 
