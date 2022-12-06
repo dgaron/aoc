c06f6867738585728161faf8ffaa383127b3a3fe
DGaron <dgaron@colostate.edu>
2022-12-05 22:57:22 -0700

    mv readFile b/c I plan to torture myself again

--- src/day5/boboMakefile.sh
+++ src/day5/boboMakefile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ -o main main.cc readFile.cc -Wall -Wextra -Wpedantic -std=c++17
+g++ -o main -I.. main.cc ../readFile.cc -Wall -Wextra -Wpedantic -std=c++17
 
 echo "I compiled your program, $USER"
